d17623f9d888ab1eed747c61044ab1b76c2279b3
vp <notejde@gmail.com>
2017-01-04 21:51:42 -0800

    add setup accountcontroller

--- src/LetzFly/Controllers/AccountController.cs
+++ src/LetzFly/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LetzFly.Models;
+using Microsoft.AspNetCore.Identity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +13,20 @@ namespace LetzFly.Controllers
     public class AccountController : Controller
     {
         // GET: /<controller>/
+        private readonly UserDbContext _db;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<UserRole> _roleManager;
+
+        public AccountController(UserManager<User> userManger, SignInManager<User> signInManager, RoleManager<UserRole> roleManager, UserDbContext db)
+        {
+            _userManager = userManger;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _db = db;
+
+        }
+
         public IActionResult Index()
         {
             return View();
