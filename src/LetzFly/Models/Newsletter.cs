baa3c829c77267dcaf3eb043566669ce3ff21c82
vp <notejde@gmail.com>
2017-01-05 12:42:29 -0800

    add newsletter model

---
+++ src/LetzFly/Models/Newsletter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetzFly.Models
+{
+    [Table("Newsletters")]
+    public class Newsletter
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image_url { get; set; }
+
+        [MinLength(10)]
+        [MaxLength(5000)]
+        public string Description { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
