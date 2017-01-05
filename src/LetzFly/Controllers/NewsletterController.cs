57423a2d36482bff29e22553e292b25a69b33125
vp <notejde@gmail.com>
2017-01-05 13:00:47 -0800

    add Newslettercontroller

---
+++ src/LetzFly/Controllers/NewsletterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LetzFly.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LetzFly.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        // GET: /<controller>/
+        {
+        private LetzFlyDbContext _db;
+
+        public NewsletterController(LetzFlyDbContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_db.Newsletters.ToList());
+        }
+    }
+}
+}
