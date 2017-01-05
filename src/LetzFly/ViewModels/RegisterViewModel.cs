3f6a4db5cb55e20b53a4ef4cf17fe4c701517e22
vp <notejde@gmail.com>
2017-01-04 21:33:45 -0800

    add registerViewmodels

---
+++ src/LetzFly/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LetzFly.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Fist&Last Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Please enter the same password!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
+//next add register view to account folder 
