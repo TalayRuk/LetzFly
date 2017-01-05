c586b400ed7b5c6dac4f5df63406635c5e7e33c9
vp <notejde@gmail.com>
2017-01-05 12:49:50 -0800

    add subscriber model

---
+++ src/LetzFly/Models/Subscriber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetzFly.Models
+{
+    [Table("Subscribers")]
+    public class Subscriber
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public virtual User User { get; set; }
+    }
+}
