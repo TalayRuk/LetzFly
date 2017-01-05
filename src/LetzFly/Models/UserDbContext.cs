1a1947f38df2bfd10fbadb772fd2e1686c214507
vp <notejde@gmail.com>
2017-01-04 21:15:26 -0800

    add update all models

--- src/LetzFly/Models/UserDbContext.cs
+++ src/LetzFly/Models/UserDbContext.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LetzFly.Models
 {
-    public class Class
+    public class UserDbContext : IdentityDbContext<User, UserRole, string>
     {
+        public UserDbContext(DbContextOptions options) :base(options)
+        {
+            //leave this open 
+        }
+        //next setup database in appsettings.json, next configure the application to use Identity w/Entity Framework & MVC in Startup.cs
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
