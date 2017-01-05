07a01f189f142e10576d7002c3e2f38f4eb574d7
vp <notejde@gmail.com>
2017-01-04 21:15:49 -0800

    add update all models

--- src/LetzFly/Models/UserRole.cs
+++ src/LetzFly/Models/UserRole.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LetzFly.Models
 {
-    public class UserRole
+    public class UserRole : IdentityRole
     {
+        //UserRole class inherits from IdentityRole base class and provide UserRole w/ RoleName and add extra property as Description.
         public string Description { get; set; }
     }
 }
